bac74c9e9fb76cc62c2ca415bca38e16d02bf6d7
Calin Marina <calin.marina@gmail.com>
2017-08-11 12:44:46 +0100

    added comment in the sh file redirecting runners to 'mvn test'

--- acceptHeaderTest.sh
+++ acceptHeaderTest.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# you can also run 'mvn test', should have all test suite there 
+
 #Accept: application/vnd.uk.gov.hmcts.<micro-service-name>.<domain-object-name>.<...?>+json; version="^1.0.1"
 
 curl -v -H "Content-Type:text/html" -H "Accept:application/vnd.uk.gov.hmcts.test+json;version=\"1.3.3\"" -i http://localhost:8080/versions;
